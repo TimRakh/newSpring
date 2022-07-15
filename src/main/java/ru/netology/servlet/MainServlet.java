550d1b0e85f2c80e901382646cab5cc7ca26b0b6
Anatolii Vasilev <avas2012@gmail.com>
2022-07-16 01:09:03 +0400

    add ApplicationInitializer

---
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -0,0 +1,65 @@
+package ru.netology.servlet;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.JavaConfig;
+import ru.netology.controller.PostController;
+import ru.netology.exception.NotFoundException;
+import ru.netology.repository.PostRepository;
+import ru.netology.service.PostService;
+
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+
+    private PostController controller;
+    private final String pathApi = "/api/posts";
+
+    @Override
+    public void init() {
+        // отдаём класс конфигурации
+        final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
+        // получаем по классу бина
+        controller = context.getBean(PostController.class);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        // если деплоились в root context, то достаточно этого
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+
+            // primitive routing
+            if (method.equals("GET") && path.equals(pathApi)) {
+                controller.all(resp);
+                return;
+            }
+            if (method.equals("GET") && path.matches(pathApi + "/\\d+")) {
+                // easy way
+                final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+                controller.getById(id, resp);
+                return;
+            }
+            if (method.equals("POST") && path.equals(pathApi)) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals("DELETE") && path.matches(pathApi + "/\\d+")) {
+                // easy way
+                final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+                controller.removeById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
